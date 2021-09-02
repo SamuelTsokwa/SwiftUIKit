b30191d6788e2ac71bf8180862b613e70cb59888
Samuel Tsokwa <samueltsokwa@Samuels-MacBook-Pro.local>
2021-09-01 23:07:17 -0400

    up

--- Sources/SwiftUIKit/Extensions/View+Navigation.swift
+++ Sources/SwiftUIKit/Extensions/View+Navigation.swift
@@ -173,3 +173,173 @@ public struct FullscreenHelperView<Content: View>: View {
         })
     }
 }
+
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+     override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+        self.setNeedsStatusBarAppearanceUpdate()
+        
+//        let standardAppearance = UINavigationBarAppearance()
+//        standardAppearance.backgroundColor = .white
+//
+//        let compactAppearance = UINavigationBarAppearance()
+//        compactAppearance.backgroundColor = .green
+//
+//        let scrollEdgeAppearance = UINavigationBarAppearance()
+//        scrollEdgeAppearance.backgroundColor = .white
+//
+//        navigationBar.standardAppearance = standardAppearance
+//        navigationBar.compactAppearance = compactAppearance
+//        navigationBar.scrollEdgeAppearance = scrollEdgeAppearance
+    }
+     override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
+public struct NavigationBarModifier: ViewModifier {
+        
+    public var backgroundColor: UIColor?
+    public var titleColor: UIColor?
+
+    public init(backgroundColor: UIColor?, titleColor: UIColor?) {
+            self.backgroundColor = backgroundColor
+            let coloredAppearance = UINavigationBarAppearance()
+            coloredAppearance.configureWithTransparentBackground()
+            coloredAppearance.backgroundColor = backgroundColor
+            coloredAppearance.titleTextAttributes = [.foregroundColor: titleColor ?? .white]
+            coloredAppearance.largeTitleTextAttributes = [.foregroundColor: titleColor ?? .white]
+
+            UINavigationBar.appearance().standardAppearance = coloredAppearance
+            UINavigationBar.appearance().compactAppearance = coloredAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+        }
+
+    public func body(content: Content) -> some View {
+            ZStack{
+                content
+                VStack {
+                    GeometryReader { geometry in
+                        Color(self.backgroundColor ?? .clear)
+                            .frame(height: geometry.safeAreaInsets.top)
+                            .edgesIgnoringSafeArea(.top)
+                        Spacer()
+                    }
+                }
+            }
+        }
+}
+public extension View {
+ 
+    public func navigationBarColor(backgroundColor: UIColor?, titleColor: UIColor?) -> some View {
+            self.modifier(NavigationBarModifier(backgroundColor: backgroundColor, titleColor: titleColor))
+        }
+
+}
+
+public class NavBarCustomizer: ObservableObject {
+    @Published public var backgroundColor: UIColor = .clear
+    @Published public var height : CGFloat = 0
+}
+
+public struct NavBarBackgroundColor: EnvironmentKey {
+    public static var defaultValue: UIColor = .clear
+}
+
+public extension EnvironmentValues {
+    public var navbarBackgroundColor: UIColor {
+        get { self[NavBarBackgroundColor.self] }
+        set { self[NavBarBackgroundColor.self] = newValue }
+    }
+}
+
+public struct NavigationConfigurator: UIViewControllerRepresentable {
+    public var configure: (UINavigationController) -> Void = { _ in }
+
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
+    }
+    public func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let nc = uiViewController.navigationController {
+            self.configure(nc)
+        }
+    }
+
+}
+public struct NavBarAccessor: UIViewControllerRepresentable {
+    public var callback: (UINavigationBar) -> Void
+    public let proxyController = ViewController()
+
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<NavBarAccessor>) ->
+                              UIViewController {
+        proxyController.callback = callback
+        return proxyController
+    }
+
+    public func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavBarAccessor>) {
+    }
+
+    public typealias UIViewControllerType = UIViewController
+
+    public class ViewController: UIViewController {
+        public var callback: (UINavigationBar) -> Void = { _ in }
+
+        public override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            if let navBar = self.navigationController {
+                self.callback(navBar.navigationBar)
+            }
+        }
+    }
+}
+public struct BarBackButton: ViewModifier {
+    @Environment(\.presentationMode) var presentation
+    public func body(content: Content) -> some View {
+        content
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action: { presentation.wrappedValue.dismiss() }) {
+              Image(systemName: "chevron.left")
+                .foregroundColor(.black)
+                .imageScale(.large)
+            })
+    }
+}
+
+public extension View {
+    public func barBackButton() -> some View {
+        self.modifier(BarBackButton())
+    }
+}
+
+public extension UITabBarController {
+    override open func viewDidLoad() {
+        
+        super.viewDidLoad()
+        print("here tab")
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        tabBar.standardAppearance = appearance
+    }
+}
+
+
+extension UITabBarController {
+    override open func viewDidAppear(_ animated: Bool) {
+        
+        
+        super.viewDidAppear(animated)
+        print("here tab r")
+        let standardAppearance = UITabBarAppearance()
+
+        standardAppearance.configureWithOpaqueBackground()
+        
+        standardAppearance.configureWithTransparentBackground()
+
+        tabBar.standardAppearance = standardAppearance
+    }
+}
